c71ff54f13be5f3e9e055110ea397b693b400121
Terriuawara Maguir <terriuawara@gmail.com>
2025-03-19 14:30:26 +0500

    Добавьте файлы проекта.

---
+++ LanguageSchoolTR14WR/LanguageSchoolTR14WR/PeoplePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LanguageSchoolTR14WR
+{
+  /// <summary>
+  /// Логика взаимодействия для PeoplePage.xaml
+  /// </summary>
+  public partial class PeoplePage : Page
+  {
+    public PeoplePage()
+    {
+      InitializeComponent();
+
+      var currentPeople = MRLanguageSchoolEntities.GetContext().Client.ToList();
+
+      PeopleLV.ItemsSource = currentPeople;
+    }
+  }
+}
